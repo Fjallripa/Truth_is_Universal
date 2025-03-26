bdd58aed16fc86e2be20c3cba285a4c06b46d821
ab227@uni-heidelberg.de <hd_ab227@uc2n994.localdomain>
2025-03-26 10:26:54 +0100

    added load-llm.py script

---
+++ load-llm.py
@@ -0,0 +1,15 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+repository_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+#repository_name = "roneneldan/tinystories-1M"
+print(f"Loading {repository_name} model and tokenizer.")
+tokenizer = AutoTokenizer.from_pretrained(repository_name)
+model = AutoModelForCausalLM.from_pretrained(repository_name)
+
+save_dir = "../TiU_data/model_weights/llama3_8b_chat_hf"
+#save_dir = "../TiU_data/model_weights/tinystories_1m_hf"
+print(f"Saving both to {save_dir}.")
+tokenizer.save_pretrained(save_dir)
+model.save_pretrained(save_dir)
+
+print("Done.")
